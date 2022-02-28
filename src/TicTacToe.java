9a747778f62cc28a22a5134408de8352e19971c8
TyrkiskPeber <77719459+TyrkiskPeber@users.noreply.github.com>
2022-02-28 11:18:28 +0100

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,6 @@
+public class TicTacToe {
+    public static void main(String[] args){
+    char[][] spelplan = { {'a','b','c'}, {'d','e','f'}, {'g','h','i'} };
+    System.out.println(spelplan[0][2]);
+}
+}
